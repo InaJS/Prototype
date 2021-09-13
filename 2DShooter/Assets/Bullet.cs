98c0363e7220b88ed44df0cbd1d24c87d235bc48
InaJS <esalmenranta@gmail.com>
2021-09-13 11:46:03 +0200

    Bullet collision
    Bullets have collision, and can now hit things.

---
+++ 2DShooter/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //Triggers when collides with something
+
+    //Instantiate the Hit Effect
+    public GameObject HitEffect;
+    void OnCollisionEnter2D(Collision2D Collision)
+    {
+        GameObject Effect = Instantiate(HitEffect, transform.position, Quaternion.identity);
+        //Quaternion.Identity being the default rotation.
+        Destroy(Effect, 5f);
+        Destroy(gameObject);
+    }
+}
