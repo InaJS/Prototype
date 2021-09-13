af06643e4bbb87d3fac573fe1b4b3a28dcc8fcc7
InaJS <esalmenranta@gmail.com>
2021-09-13 09:52:56 +0200

    PlayerMovement
    Why the hell won't this work?

---
+++ 2DShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+//Had to write that it's supposed to be using the input system.
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Player input with the new input system.
+
+    [SerializeField]
+    private float Speed;
+    private float Horizontal;
+    private float Vertical;
+
+//An update checks every frame if the action (The key is pressed) is active.
+    private void update() 
+    {
+        transform.localPosition += Vector3.right * Speed * Horizontal;
+        transform.localPosition += Vector3.up * Speed * Vertical;
+    }
+    
+    //Player movement
+    public void Move(InputAction.CallbackContext Move)
+    { 
+        Horizontal = Move.ReadValue<Vector2>().x;
+        Vertical = Move.ReadValue<Vector2>().y;
+    }
+}
+
+//In PlayerInput in the inspector for the player. Events-Player-Move. Put this in the Move(CallbackContext)
+//And the function as playerMovement-Move.
