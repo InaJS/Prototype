9f1c763dba5c5ab414bcbe0fd9803821e78263f6
InaJS <esalmenranta@gmail.com>
2021-09-14 11:12:51 +0200

    Main Menu added

---
+++ 2DShooter/Assets/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//To load a new scene, UnityEngine.SceneManagement needs to be in use.
+public class Menu : MonoBehaviour
+{
+   public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+
+    }
+
+    public void Continue()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    //Quit the Application.
+    public void Quit()
+    {
+        //Won't happen in the Editor. So to make sure it's working.
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
