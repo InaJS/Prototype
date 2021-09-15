39dff59860d7e56509c2057b49878950e1c80537
InaJS <esalmenranta@gmail.com>
2021-09-15 11:11:29 +0200

    Update EnemyScore(Paste in Enemy script).cs

--- 2DShooter/Assets/Scripts/EnemyScore(Paste in Enemy script).cs
+++ 2DShooter/Assets/Scripts/EnemyScore(Paste in Enemy script).cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScorePasteinEnemyscript : MonoBehaviour
+
+//Paste this script into the enemy script once it's been established.
 {
     void OnCollisionEnter2D (Collision2D Collision)
     {
